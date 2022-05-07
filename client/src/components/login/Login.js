cc9a2b9981ab30e1bce72720196bc8d2bf2fd11f
risha-parveen <rishaparveen12@gmail.com>
2022-05-07 14:23:59 +0530

    login page styling

--- client/src/components/login/Login.js
+++ client/src/components/login/Login.js
@@ -6,13 +6,24 @@ const Login = () => {
     <div className="login">
       <div className="overview">
         <h1>Expense Tracker</h1>
-        <h3>Track your expenses in an effiecient way</h3>
+        <h4>Track your expenses in an effiecient way</h4>
       </div>
       <div className="ellipse"></div>
       <div className="login-board">
         <form>
-          <input className="username" placeholder="username" />
-          <input className="password" placeholder="password" />
+          <input
+            className="username fields"
+            placeholder="username"
+            spellcheck="false"
+            autocomplete="off"
+          />
+          <input
+            className="password fields"
+            type="password"
+            placeholder="password"
+            spellcheck="false"
+            autocomplete="off"
+          />
           <button className="login-button">Log In</button>
         </form>
       </div>
