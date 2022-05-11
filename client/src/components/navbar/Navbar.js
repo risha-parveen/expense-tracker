e59793a0a49830779eae9643cd956e0ce7db6400
risha-parveen <rishaparveen12@gmail.com>
2022-05-11 07:40:15 +0530

    update

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -1,16 +1,54 @@
 import React from "react";
 import "./Navbar.css";
 import { FaAlignJustify } from "react-icons/fa";
+import { BiChevronDown, BiChevronUp } from "react-icons/bi";
+import { useState } from "react";
+
+const Sidebar = () => {
+  return <div className="main-sidebar"></div>;
+};
 
 function Navbar() {
+  const [upDown, setUpdown] = useState(false);
+
+  const toggleUpDown = () => {
+    setUpdown(!upDown);
+  };
+
   return (
-    <div className="main-nav">
-      <div className="left-nav">
-        <button className="expand-button">
-          <FaAlignJustify color="white" fontSize="2rem" />
-        </button>
+    <div className="navbar">
+      <div className="main-nav">
+        <div className="left-nav">
+          <button className="expand-button">
+            <FaAlignJustify color="white" fontSize="1.5rem" />
+          </button>
+          <p>Expense Tracker</p>
+        </div>
+        <div className="right-nav">
+          <button
+            className="arrow-down-button arrow-button"
+            id={upDown ? "close" : "open"}
+            onClick={(e) => {
+              e.preventDefault();
+              toggleUpDown();
+            }}
+          >
+            <BiChevronDown color="white" fontSize="1.5rem" />
+          </button>
+          <button
+            className="arrow-up-button arrow-button"
+            id={upDown ? "open" : "close"}
+            onClick={(e) => {
+              e.preventDefault();
+              toggleUpDown();
+            }}
+          >
+            <BiChevronUp color="white" fontSize="1.5rem" />
+          </button>
+          <p className="account-username">Adithya Raj</p>
+        </div>
       </div>
-      <div className="right-nav"></div>
+      <Sidebar />
     </div>
   );
 }
--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -2,7 +2,11 @@ import React from "react";
 import Navbar from "../../components/navbar/Navbar";
 
 function Home() {
-  return <Navbar />;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 }
 
 export default Home;
