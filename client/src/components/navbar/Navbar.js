4c484b2899a1b6786c0586e751190c82a453ee6b
risha-parveen <rishaparveen12@gmail.com>
2022-05-13 00:57:53 +0530

    updates

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -1,63 +1,26 @@
 import React from "react";
 import "./Navbar.css";
-import { FaAlignJustify, FaUserAlt } from "react-icons/fa";
+import { FaAlignJustify } from "react-icons/fa";
 import { BiChevronDown, BiChevronUp } from "react-icons/bi";
 import { useState } from "react";
-import { Link } from "react-router-dom";
-import { AiFillHome, AiFillSignal } from "react-icons/ai";
+import Sidebar from "../sidebar/Sidebar";
 
-const SidebarData = [
-  {
-    title: "Home",
-    path: "/home",
-    icon: <AiFillHome color="white" fontSize="1.5rem" />,
-    cName: "sidebar-text",
-  },
-  {
-    title: "Expense",
-    path: "#",
-    icon: <AiFillSignal color="white" fontSize="1.5rem" />,
-    cName: "sidebar-text",
-  },
-  {
-    title: "Account",
-    path: "/",
-    icon: <FaUserAlt color="white" fontSize="1.5rem" />,
-    cName: "sidebar-text",
-  },
-];
-
-const Sidebar = ({ activity }) => {
-  return (
-    <div className="main-sidebar" id={activity}>
-      <ul className="side-menu-items">
-        <li>
-          {SidebarData.map((item, index) => {
-            return (
-              <li key={index} className={item.cName}>
-                <Link to={item.path}>
-                  {item.icon}
-                  <span className="span-text">{item.title}</span>
-                </Link>
-              </li>
-            );
-          })}
-        </li>
-      </ul>
-    </div>
-  );
-};
+let sidebar,
+  setSidebar,
+  sidebarStack = [];
 
 function Navbar() {
   const [upDown, setUpdown] = useState(false);
-  const [sidebar, setSidebar] = useState(false);
-
-  const toggleSidebar = () => {
-    setSidebar(!sidebar);
-  };
+  [sidebar, setSidebar] = useState(false);
 
   const toggleUpDown = () => {
     setUpdown(!upDown);
+    sidebarStack.push(upDown);
+  };
+
+  const toggleSidebar = () => {
+    setSidebar(!sidebar);
+    sidebarStack = sidebar;
   };
 
   return (
@@ -99,9 +62,10 @@ function Navbar() {
           <p className="account-username">Adithya Raj</p>
         </div>
       </div>
-      <Sidebar activity={sidebar ? "active" : "inactive"} />
     </div>
   );
 }
+
+export { sidebarStack };
 
 export default Navbar;
--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,11 +1,21 @@
 import React from "react";
 import Navbar from "../../components/navbar/Navbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import "./Home.css";
+import { sidebarStack } from "../../components/navbar/Navbar";
+let sidebar = null;
+const bleh = () => {
+  console.log(sidebarStack);
+};
 
 function Home() {
   return (
-    <>
+    <div className="home-page" onClick={bleh}>
       <Navbar />
-    </>
+      <div className="content-space">
+        <Sidebar activity={sidebarStack ? "active" : "inactive"} />
+      </div>
+    </div>
   );
 }
 
