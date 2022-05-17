3b2d466227525f73f8ba617a91ffce7129b9edb2
risha-parveen <rishaparveen12@gmail.com>
2022-05-17 07:01:30 +0530

    added files

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/login/Login";
+import Home from "./pages/home/Home";
+import "./App.css";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div className="expense-tracker">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />}></Route>
+          <Route path="home" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ app/src/pages/login/Login.js
@@ -0,0 +1,93 @@
+import { useState } from "react";
+import "./Login.css";
+import { Link } from "react-router-dom";
+
+const LoginPage = () => {
+  const [form, setform] = useState(true);
+
+  const toggleForm = () => {
+    setform(!form);
+  };
+
+  return (
+    <div className="login">
+      <div className="overview">
+        <h1>Expense Tracker</h1>
+        <h4>Track your expenses in an effiecient way</h4>
+      </div>
+      <div className="ellipse"></div>
+      <div className="login-board">
+        <form className="login-form" id={form ? "open" : "close"}>
+          <input
+            className="username fields"
+            placeholder="username"
+            spellCheck="false"
+            autoComplete="off"
+          />
+          <input
+            className="password fields"
+            type="password"
+            placeholder="password"
+            spellCheck="false"
+            autoComplete="off"
+          />
+          <Link to="/home">
+            <button className="login-button">Log In</button>
+          </Link>
+          <button
+            className="create-new-account"
+            onClick={(e) => {
+              e.preventDefault();
+              toggleForm();
+            }}
+          >
+            new here? Sign Up
+          </button>
+        </form>
+
+        <form className="signup-form" id={form ? "close" : "open"}>
+          <input
+            className="username fields"
+            placeholder="username"
+            spellCheck="false"
+            autoComplete="off"
+          />
+          <input
+            className="password fields"
+            type="password"
+            placeholder="password"
+            spellCheck="false"
+            autoComplete="off"
+          />
+          <input
+            className="confirm-password fields"
+            type="password"
+            placeholder="confirm password"
+            spellCheck="false"
+            autoComplete="off"
+          />
+          <button
+            className="signup-button"
+            onClick={(e) => {
+              e.preventDefault();
+              toggleForm();
+            }}
+          >
+            Sign Up
+          </button>
+          <button
+            className="back-to-login"
+            onClick={(e) => {
+              e.preventDefault();
+              toggleForm();
+            }}
+          >
+            Back to log in
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
