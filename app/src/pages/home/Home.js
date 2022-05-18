4ba3ba9a6b0482d5935ce8d3a419eb33fa651ffc
risha-parveen <rishaparveen12@gmail.com>
2022-05-18 06:51:45 +0530

    update

--- app/src/pages/home/Home.js
+++ app/src/pages/home/Home.js
@@ -1,7 +1,20 @@
 import React from "react";
+import "./Home.css";
+import { Layout, Menu } from "antd";
+
+const { Header, Footer, Content, Sider } = Layout;
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <Layout>
+      <Header>Header</Header>
+      <Layout>
+        <Sider>Sider</Sider>
+        <Content>Content</Content>
+      </Layout>
+      <Footer>Footer</Footer>
+    </Layout>
+  );
 }
 
 export default Home;
