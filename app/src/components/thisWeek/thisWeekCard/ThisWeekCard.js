2f825dc903e644b589507bc35d20a9134e4c698a
risha-parveen <rishaparveen12@gmail.com>
2022-06-09 19:27:43 +0530

    added revenue and expense button

--- app/src/components/thisWeek/thisWeekCard/ThisWeekCard.js
+++ app/src/components/thisWeek/thisWeekCard/ThisWeekCard.js
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function ThisWeekCard(props) {
+  const [open, setOpen] = useState(true);
+
+  const toggleDisplay = () => {
+    setOpen(!open);
+  };
+
   if (props.activity === "inactive") {
     let id = "display-non-hidden";
     return (
@@ -10,7 +16,6 @@ function ThisWeekCard(props) {
           <p className="day-of-the-week">{props.dayOfWeek}</p>
         </div>
         {(() => {
-          //console.log(props.currentDay, props.nowDate);
           if (props.dateNumber < props.nowDate) id = "display-non-hidden";
           else id = "display-hidden";
         })()}
@@ -21,19 +26,40 @@ function ThisWeekCard(props) {
       </div>
     );
   } else {
+    let id = "display-non-hidden";
     return (
-      <div className="card" key={props.index} id={props.activity}>
+      <div
+        className="card"
+        key={props.index}
+        id={props.activity}
+        onClick={(e) => {
+          e.preventDefault();
+          toggleDisplay();
+        }}
+      >
         <div className="left-part">
           <p className="date-of-the-week">{props.dateOfWeek}</p>
           <p className="day-of-the-week">{props.dayOfWeek}</p>
         </div>
         <div className="right-part">
-          <p className="expense-tag">Add Expenditure</p>
+          <p
+            className="expense-tag"
+            id={open ? "display-non-hidden" : "display-hidden"}
+          >
+            Add Expenditure
+          </p>
+          <div
+            className="right-part"
+            id={open ? "display-hidden" : "display-non-hidden"}
+          >
+            <button className="revenue-button">Revenue</button>
+            <button className="expense-button">Expense</button>
+            <button className="green-tick"></button>
+          </div>
         </div>
       </div>
     );
   }
-  
 }
 
 export default ThisWeekCard;
