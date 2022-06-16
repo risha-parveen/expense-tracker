d570d15be2e338954a4aba8ac5132a3ee280cf0b
risha-parveen <rishaparveen12@gmail.com>
2022-06-16 07:34:12 +0530

    add expense api

--- server/routes/expenseRoute.js
+++ server/routes/expenseRoute.js
@@ -15,10 +15,17 @@ const expense_db = require("../models/expense_schema");
 const user_db = require("../models/user_schema");
 const info_db = require("../models/info_schema");
 
+const currency_symbol = {
+  dollar: "$",
+  euro: "€",
+  rupee: "₹",
+};
+
 //username
 //revenue
 //expense
 //date
+//currency
 
 router.post("/add_expense", async (req, res) => {
   try {
@@ -31,27 +38,59 @@ router.post("/add_expense", async (req, res) => {
     });
     return;
   }
-  let today_expense = {
-    date: req.body.date,
-    revenue: req.body.revenue,
-    expense: req.body.expense,
-  };
-  result[0].expense_info.push(today_expense);
-  try {
-    await expense_db.replaceOne({ username: req.body.username }, result[0], {
-      upsert: true,
-    });
-    res.status(200).send({
-      action: "updated",
-      success: true,
-    });
-  } catch (error) {
-    console.log(error);
-    res.status(500).send({
-      action: "not updated",
-      success: false,
-    });
-    return;
+  let rev = req.body.revenue + currency_symbol[req.body.currency];
+  let exp = req.body.expense + currency_symbol[req.body.currency];
+
+  let today_expense = result[0].expense_info.pop();
+  if (today_expense === undefined || today_expense.date !== req.body.date) {
+    //if the expense is newly added for the day
+    if (today_expense != undefined) {
+      result[0].expense_info.push(today_expense);
+    }
+
+    let new_expense = {
+      date: req.body.date,
+      revenue: rev,
+      expense: exp,
+    };
+    result[0].expense_info.push(new_expense);
+    try {
+      await expense_db.replaceOne({ username: req.body.username }, result[0], {
+        upsert: true,
+      });
+      res.status(200).send({
+        action: "added todays expense",
+        success: true,
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).send({
+        action: "not added",
+        success: false,
+      });
+      return;
+    }
+  } else {
+    //if the expense for today is to be edited
+    today_expense.revenue = rev;
+    today_expense.expense = exp;
+    result[0].expense_info.push(today_expense);
+    try {
+      await expense_db.replaceOne({ username: req.body.username }, result[0], {
+        upsert: true,
+      });
+      res.status(200).send({
+        action: "edited todays expense",
+        success: true,
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).send({
+        action: "not edited",
+        success: false,
+      });
+      return;
+    }
   }
 });
 
